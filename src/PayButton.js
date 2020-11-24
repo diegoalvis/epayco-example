32294d5559d091b3e4ee72d4c7a574ea5bd4a0e2
Diego Alvis <diegoalvispal@gmail.com>
2020-11-24 23:16:06 +0100

    Implement epayco

---
+++ src/PayButton.js
@@ -0,0 +1,106 @@
+import React, { useCallback } from 'react';
+ 
+function PayButton()  {
+
+  var epayco = require('epayco-sdk-node')({
+    apiKey: '65afb5be48a6a50793901a14bc038c2f',
+    privateKey: 'ee31485514614e5619a17f21327c6ab5',
+    lang: 'ES',
+    test: true
+  })
+  
+  var credit_info = {
+    "card[number]": "4575623182290326",
+    "card[exp_year]": "2025",
+    "card[exp_month]": "12",
+    "card[cvc]": "123"
+  }
+  
+  epayco.token.create(credit_info)
+    .then(function(token) {
+        console.log(token);
+    })
+    .catch(function(err) {
+        console.log("err: " + err);
+    });
+
+  
+  function handlePay(e) {
+    console.log('this is:', this);
+
+    var payment_info = {
+      token_card: "token_id",
+      customer_id: "customer_id",
+      doc_type: "CC",
+      doc_number: "1035851980",
+      name: "John",
+      last_name: "Doe",
+      email: "example@email.com",
+      city: "Bogota",
+      address: "Cr 4 # 55 36",
+      phone: "3005234321",
+      cell_phone: "3010000001",
+      bill: "OR-1234",
+      description: "Test Payment",
+      value: "116000",
+      tax: "16000",
+      tax_base: "100000",
+      currency: "COP",
+      dues: "12",
+      ip:"190.000.000.000", /*This is the client's IP, it is required */
+      url_response: "https://ejemplo.com/respuesta.html",
+      url_confirmation: "https://ejemplo.com/confirmacion",
+      method_confirmation: "GET",
+  
+      //Extra params: These params are optional and can be used by the commerce
+  
+      use_default_card_customer: true,/*if the user wants to be charged with the card that the customer currently has as default = true*/
+     
+     extras: {
+          extra1: "",
+          extra2: "",
+          extra3: "",
+          extra4: "",
+          extra5: "",
+          extra6: ""
+      }
+  }
+
+  epayco.charge.create(payment_info)
+      .then(function(charge) {
+          console.log(charge);
+      })
+      .catch(function(err) {
+          console.log("err: " + err);
+      });
+  
+    }
+      
+ 
+  return (
+    <form>
+    <script src='https://checkout.epayco.co/checkout.js' 
+        data-epayco-key='65afb5be48a6a50793901a14bc038c2f' 
+        className='epayco-button' 
+        data-epayco-amount='142800' 
+        data-epayco-tax='22800'
+        data-epayco-tax-base='120000'
+        data-epayco-name='Solicitud de anfitrion' 
+        data-epayco-description='Solicitud de anfitrion' 
+        data-epayco-currency='cop'    
+        data-epayco-country='CO' 
+        data-epayco-test='true' 
+        data-epayco-external='true' 
+        data-epayco-response=''  
+        data-epayco-confirmation='' 
+        data-epayco-button='https://369969691f476073508a-60bf0867add971908d4f26a64519c2aa.ssl.cf5.rackcdn.com/btns/boton_carro_de_compras_epayco4.png'> 
+    </script> 
+</form> 
+    // <button onClick={handlePay}>
+    //   Demo pay button
+    // </button>
+  )
+};
+
+export default PayButton;
+
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import PayButton from './PayButton';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <PayButton />
   </React.StrictMode>,
   document.getElementById('root')
 );
