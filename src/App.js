5666b9a02cf835667e328702da0daa0f573c52f2
Diego Alvis <diegoalvispal@gmail.com>
2020-11-25 13:29:17 +0100

    Trigger deploy in heroku

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import useScript from './useScript';
 
 function App() {
+
+  useScript('');
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +22,7 @@ function App() {
           Learn React
         </a>
       </header>
+    
     </div>
   );
 }
---
+++ src/useScript.js
@@ -0,0 +1,38 @@
+import { useEffect } from 'react';
+
+
+const useScript = url => {
+
+
+  useEffect(() => {
+    const proxyurl = "https://cors-anywhere.herokuapp.com/";  
+    const url = 'https://checkout.epayco.co/checkout.js';
+  
+    const script = document.createElement('script');
+    script.src = proxyurl + url;
+    script.async = true;
+    script.setAttribute('data-epayco-key', '65afb5be48a6a50793901a14bc038c2f');
+    script.setAttribute('class', 'epayco-button');
+    script.setAttribute('data-epayco-amount', '142800');
+    script.setAttribute('data-epayco-tax', '22800');
+    script.setAttribute('data-epayco-tax-base', '120000');
+    script.setAttribute('data-epayco-name', 'Solicitud de anfitrion');
+    script.setAttribute('data-epayco-description', 'Solicitud de anfitrion');
+    script.setAttribute('data-epayco-currency', 'cop');
+    script.setAttribute('data-epayco-country', 'CO');
+    script.setAttribute('data-epayco-test', 'true');
+    script.setAttribute('data-epayco-external', 'true');
+    script.setAttribute('data-epayco-response', '');
+    script.setAttribute('data-epayco-confirmation', '');
+    script.setAttribute('data-epayco-button', 'https://369969691f476073508a-60bf0867add971908d4f26a64519c2aa.ssl.cf5.rackcdn.com/btns/boton_carro_de_compras_epayco4.png');
+
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    }
+  }, [url]);
+};
+
+export default useScript;
